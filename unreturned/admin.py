c7e95b15be71213bdb65a3269ce613697eac42e1
JWestinP <Javeloza.Westin@gmail.com>
2023-12-19 02:29:27 +0800

    Models for recents and unreturned logs

--- unreturned/admin.py
+++ unreturned/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Unreturned_Item
 
 # Register your models here.
+class Unreturned_Items_Admin(admin.ModelAdmin):
+    list_display = ['item_id', 
+                    'item_name', 
+                    'item_quantity', 
+                    'item_date_borrowed',
+                    'item_days_not_returned']
+    
+admin.site.register(Unreturned_Item, Unreturned_Items_Admin)
--- unreturned/models.py
+++ unreturned/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Unreturned_Item(models.Model):
+    item_id = models.IntegerField(primary_key = True)
+    item_name = models.CharField(max_length = 128)
+    item_quantity = models.IntegerField()
+    item_date_borrowed = models.DateField()
+    item_days_not_returned = models.IntegerField()
