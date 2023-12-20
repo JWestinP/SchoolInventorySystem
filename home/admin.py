69521e33eca0a10c07a892dabb80626d2c8ea82a
West <Javeloza.Westin@gmail.com>
2023-12-21 02:56:13 +0800

    Inventory model setup

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Furniture, Room, Cleaning_Material, Technology
 
 # Register your models here.
+class Furniture_Admin(admin.ModelAdmin):
+    list_display = ['item_id',
+                    'item_name',
+                    'item_category',
+                    'item_description',
+                    'item_total_quantity',
+                    'item_current_quantity',
+                    'item_borrowed_quantity']
+
+class Rooms_Admin(admin.ModelAdmin):
+    list_display = ['item_id',
+                    'item_name',
+                    'item_category',
+                    'item_description',
+                    'item_total_quantity',
+                    'item_current_quantity',
+                    'item_borrowed_quantity']
+
+class Cleaning_Materials_Admin(admin.ModelAdmin):
+    list_display = ['item_id',
+                    'item_name',
+                    'item_category',
+                    'item_description',
+                    'item_total_quantity',
+                    'item_current_quantity',
+                    'item_borrowed_quantity']
+
+class Technology_Admin(admin.ModelAdmin):
+    list_display = ['item_id',
+                    'item_name',
+                    'item_category',
+                    'item_description',
+                    'item_total_quantity',
+                    'item_current_quantity',
+                    'item_borrowed_quantity']
+
+admin.site.register(Furniture, Furniture_Admin)
+admin.site.register(Room, Rooms_Admin)
+admin.site.register(Cleaning_Material, Cleaning_Materials_Admin)
+admin.site.register(Technology, Technology_Admin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class Furniture(models.Model):
+    item_id = models.IntegerField(primary_key = True)
+    item_name = models.CharField(max_length = 128)
+    item_category = models.CharField(max_length = 128)
+    item_description = models.CharField(max_length = 256)
+    item_total_quantity = models.IntegerField()
+    item_current_quantity = models.IntegerField()
+    item_borrowed_quantity = models.IntegerField()
+    
+class Room(models.Model):
+    item_id = models.IntegerField(primary_key = True)
+    item_name = models.CharField(max_length = 128)
+    item_category = models.CharField(max_length = 128)
+    item_description = models.CharField(max_length = 256)
+    item_total_quantity = models.IntegerField()
+    item_current_quantity = models.IntegerField()
+    item_borrowed_quantity = models.IntegerField()
+
+class Cleaning_Material(models.Model):
+    item_id = models.IntegerField(primary_key = True)
+    item_name = models.CharField(max_length = 128)
+    item_category = models.CharField(max_length = 128)
+    item_description = models.CharField(max_length = 256)
+    item_total_quantity = models.IntegerField()
+    item_current_quantity = models.IntegerField()
+    item_borrowed_quantity = models.IntegerField()
+
+class Technology(models.Model):
+    item_id = models.IntegerField(primary_key = True)
+    item_name = models.CharField(max_length = 128)
+    item_category = models.CharField(max_length = 128)
+    item_description = models.CharField(max_length = 256)
+    item_total_quantity = models.IntegerField()
+    item_current_quantity = models.IntegerField()
+    item_borrowed_quantity = models.IntegerField()    
