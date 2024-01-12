37abbce397008a47038bb28135868d69bd713a99
-ByStander <kulormanibo@gmail.com>
2024-01-12 09:27:23 +0900

    update
    -implement return item in admin

--- unreturned/urls.py
+++ unreturned/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("unreturned", views.unreturned, name="unreturned"),
     path("admin_unreturned", views.admin_unreturned, name="admin_unreturned"),
-    path('return_item/<item_id>/<item_stock_id>', views.return_item, name="return_item")
+    path('return_item/<item_id>/<item_stock_id>', views.return_item, name="return_item"),
+    path('return_item/<item_id>/<item_stock_id>', views.admin_return_item, name="admin_return_item")
 ]
