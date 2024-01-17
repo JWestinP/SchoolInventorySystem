9a0c9561709f9d1a99262347be561fb98a031d3f
West <Javeloza.Westin@gmail.com>
2024-01-17 21:20:28 +0800

    Added member view

--- members/views.py
+++ members/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from home.decorators import allowed_users
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 @allowed_users(allowed_roles=['Admin'])
 def members(request):
-    return render(request, ('members/members.html'))
+    faculty = User.objects.all()
+    return render(request, ('members/members.html'),{'faculty': faculty})
