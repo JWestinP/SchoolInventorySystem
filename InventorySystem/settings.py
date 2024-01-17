34b8adb465a3c2e8658e79702ff927c55903f6ff
West <Javeloza.Westin@gmail.com>
2024-01-17 21:33:58 +0800

    Updated forgot pass sender

--- InventorySystem/settings.py
+++ InventorySystem/settings.py
@@ -151,6 +151,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'westin.javeloza@gmail.com'
-EMAIL_HOST_PASSWORD = 'cnoveqiigfavrgsy'
+EMAIL_HOST_USER = 'lspuinventorysystem@gmail.com'
+EMAIL_HOST_PASSWORD = 'jhqcdveminrnmgjp'
+
 
