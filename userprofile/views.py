cd773ac8b597c3d3fe98099b65cf7f9351217c46
West <Javeloza.Westin@gmail.com>
2024-01-19 03:02:51 +0800

    update

--- userprofile/views.py
+++ userprofile/views.py
@@ -11,32 +11,9 @@ from home.decorators import allowed_users
 @login_required
 @allowed_users(allowed_roles=['Faculty'])
 def userprofile(request):
-    if request.method == 'POST':
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            print("User form saved successfully!")
-            p_form.save()
-            print("Profile form saved successfully!")
+    user_profile = Profile.objects.get(user=request.user) 
 
-            messages.success(request, f'Your account has been updated!')
-            return redirect('userprofile')
-        else:
-            print("Form is not valid. Errors:", u_form.errors, p_form.errors)
-
-    else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
-
-
-    context = {
-        'u_form': u_form,
-        'p_form': p_form
-    }
-    return render(request, 'userprofile/userprofile.html', context)
+    return render(request, 'userprofile/userprofile.html',{'user_profile': user_profile})
 
 @login_required
 @allowed_users(allowed_roles=['Admin'])
@@ -45,7 +22,7 @@ def admin_userprofile(request):
     return render(request, 'userprofile/admin_userprofile.html',{'user_profile': user_profile})
 
 @login_required
-@allowed_users(allowed_roles=['Faculty', 'Admin'])
+@allowed_users(allowed_roles=['Faculty'])
 def editprofile(request):
     # Retrieve the user's profile
     #user_profile = User.objects.get(pk=request.user.id).profile
@@ -75,3 +52,35 @@ def editprofile(request):
         'p_form': p_form,
     }
     return render(request, 'userprofile/editprofile.html', context)
+
+@login_required
+@allowed_users(allowed_roles=['Admin'])
+def editprofile_admin(request):
+    # Retrieve the user's profile
+    #user_profile = User.objects.get(pk=request.user.id).profile
+    user_profile = request.user.profile
+
+
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=user_profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+
+             # Check user's group and redirect accordingly
+            if request.user.groups.filter(name='Admin').exists():
+                return redirect('admin_userprofile')
+            else:
+                return redirect('userprofile')
+            #return redirect('userprofile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=user_profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'userprofile/editprofileadmin.html', context)
