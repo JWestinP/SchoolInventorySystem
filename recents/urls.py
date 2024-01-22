00a35c34410ae35444f2fb3a5f3f767cec03bee7
Kimbryan04 <khayebie04@gmail.com>
2024-01-22 12:47:30 +0800

    added search function in recents and unreturned

--- recents/urls.py
+++ recents/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('recents', views.recents, name='recents'),
     path('admin_recents', views.admin_recents, name='admin_recents'),
+    path('admin_search_recents', views.admin_search_recents, name='admin_search_recents'),
+    path('faculty_search_recents', views.faculty_search_recents, name='faculty_search_recents'),
 ]
