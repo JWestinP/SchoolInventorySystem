905a92630af3c351cac1532c6388f13c64cb20dd
-ByStander <kulormanibo@gmail.com>
2024-02-09 18:13:53 +0900

    Update views.py

--- unreturned/views.py
+++ unreturned/views.py
@@ -29,25 +29,15 @@ def admin_unreturned(request):
 
 def return_item(request, item_id, item_stock_id):
 
-    # determine what item from ureturned, and compute the quantity when returning item
     unreturned_items = Unreturned_Item.objects.get(pk=item_id)
     unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + unreturned_items.item_borrowed.item_quantity
 
-    # get the item using stock id, update the item quantity and save it to database
     item = Stock.objects.get(pk = item_stock_id)
     item.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity
     item.save()
 
-    # delete the item from unreturned
     unreturned_items.delete()
 
-    # for checking
-    # print(unreturned_items.item_borrowed.item_stock.item_current_quantity)
-    # print(item)
-    # print(item.item_current_quantity)
-    # for checking
-
-    # redirect again the user to the page of "unreturned" after the process
     return redirect('unreturned')
 
 def admin_return_item(request, item_id, item_stock_id):
