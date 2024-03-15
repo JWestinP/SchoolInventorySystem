d50af17eff7d0e662c769a90d0cc128712a1111a
West <Javeloza.Westin@gmail.com>
2024-03-15 19:00:34 +0800

    update

--- members/views.py
+++ members/views.py
@@ -15,33 +15,33 @@ import json
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'  # Include all fields
-        depth = 1  # Include one level of related objects
+        fields = '__all__' 
+        depth = 1  
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
-        fields = '__all__'  # Include all fields
+        fields = '__all__'  
 
 class StockSerializer(serializers.ModelSerializer):
-    item_information = ItemSerializer()  # Nested serializer for the related Item
+    item_information = ItemSerializer()  
 
     class Meta:
         model = Stock
-        fields = '__all__'  # Include all fields
+        fields = '__all__' 
 
 class HistorySerializer(serializers.ModelSerializer):
     item_stock = StockSerializer()
     class Meta:
         model = Borrowed_Item
-        fields = '__all__'  # Include all fields
+        fields = '__all__'  
         depth = 1 
         
 class UnreturnedSerializer(serializers.ModelSerializer):
     item_borrowed = HistorySerializer()
     class Meta:
         model = Unreturned_Item
-        fields = '__all__'  # Include all fields
+        fields = '__all__' 
         depth = 1
 
 @allowed_users(allowed_roles=['Admin'])
@@ -62,7 +62,6 @@ def get_user_info(request):
                              'total_recents': user_recents_total,
                              'total_unreturned': user_unreturned_total})
     except ObjectDoesNotExist:
-        # Handle the case where the user with the specified ID doesn't exist
         return JsonResponse({'error': 'User not found'}, status=404)
     
 def get_user_history(request):
@@ -86,5 +85,4 @@ def get_user_unreturned(request):
         return JsonResponse({'user_unreturned': serialized_data})
     
     except ObjectDoesNotExist:
-        # Handle the case where the user with the specified ID doesn't exist
         return JsonResponse({'error': 'User not found'}, status=404)
