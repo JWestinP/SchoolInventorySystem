c8fec73d7b5193b8a2bd2f77b87f992ec6b4103c
-ByStander <160066226+WrapLine@users.noreply.github.com>
2024-03-22 07:13:35 +0900

    udpate patch
    -add patch to the admin_unreturned

--- unreturned/views.py
+++ unreturned/views.py
@@ -49,8 +49,14 @@ def return_item(request, item_id, item_stock_id, borrow_form_id):
 
 def admin_return_item(request, item_id, item_stock_id, borrow_form_id):
 
+    pristine_no = request.POST.get("pristine")
+    damaged_no = request.POST.get("damaged")
+
     unreturned_items = Unreturned_Item.objects.get(pk=item_id)
     unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + unreturned_items.item_borrowed.item_quantity
+    unreturned_items.item_borrowed.item_stock.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity + int(pristine_no)
+    unreturned_items.item_borrowed.item_stock.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity + int(damaged_no)
+    unreturned_items.save()
 
     borrowed_item = Borrowed_Item.objects.get(pk=borrow_form_id)
     borrowed_item.item_returned = True
