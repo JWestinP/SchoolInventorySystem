1ff8924a56f95c3628376b2c4c2f7189a4b50298
West <Javeloza.Westin@gmail.com>
2024-03-25 17:41:25 +0800

    Update

--- unreturned/views.py
+++ unreturned/views.py
@@ -6,6 +6,7 @@ from home.models import *
 from datetime import datetime
 from home.decorators import allowed_users
 from home.decorators import allowed_users
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -26,24 +27,30 @@ def admin_unreturned(request):
 
 def return_item(request, item_id, item_stock_id, borrow_form_id):
 
+    
     pristine_no = request.POST.get("pristine")
     damaged_no = request.POST.get("damaged")
 
+    total_no = int(pristine_no) + int(damaged_no)
     unreturned_items = Unreturned_Item.objects.get(pk=item_id)
-    unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + unreturned_items.item_borrowed.item_quantity
-    unreturned_items.item_borrowed.item_stock.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity + int(pristine_no)
-    unreturned_items.item_borrowed.item_stock.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity + int(damaged_no)
-    unreturned_items.save()
-
     borrowed_item = Borrowed_Item.objects.get(pk=borrow_form_id)
     borrowed_item.item_returned = True
     borrowed_item.save()
+    if total_no == borrowed_item.item_quantity:
+        unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + int(pristine_no)
+        unreturned_items.item_borrowed.item_stock.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity + int(pristine_no)
+        unreturned_items.item_borrowed.item_stock.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity + int(damaged_no)
+        unreturned_items.save()
 
-    item = Stock.objects.get(pk = item_stock_id)
-    item.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity
-    item.save()
+        
 
-    unreturned_items.delete()
+        item = Stock.objects.get(pk = item_stock_id)
+        item.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity
+        item.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity
+        item.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity
+        item.save()
+
+        unreturned_items.delete()
 
     return redirect('unreturned')
 
@@ -52,22 +59,28 @@ def admin_return_item(request, item_id, item_stock_id, borrow_form_id):
     pristine_no = request.POST.get("pristine")
     damaged_no = request.POST.get("damaged")
 
+    total_no = int(pristine_no) + int(damaged_no)
     unreturned_items = Unreturned_Item.objects.get(pk=item_id)
-    unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + int(pristine_no)
-    unreturned_items.item_borrowed.item_stock.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity + int(pristine_no)
-    unreturned_items.item_borrowed.item_stock.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity + int(damaged_no)
-    unreturned_items.save()
-
     borrowed_item = Borrowed_Item.objects.get(pk=borrow_form_id)
     borrowed_item.item_returned = True
     borrowed_item.save()
+    if total_no == borrowed_item.item_quantity:
+        unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + int(pristine_no)
+        unreturned_items.item_borrowed.item_stock.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity + int(pristine_no)
+        unreturned_items.item_borrowed.item_stock.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity + int(damaged_no)
+        unreturned_items.save()
 
-    item = Stock.objects.get(pk = item_stock_id)
-    item.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity
-    item.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity
-    item.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity
-    item.save()
+        
 
-    unreturned_items.delete()
+        item = Stock.objects.get(pk = item_stock_id)
+        item.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity
+        item.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity
+        item.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity
+        item.save()
 
-    return redirect('admin_unreturned')
+        unreturned_items.delete()
+
+        return redirect('admin_unreturned')
+    
+    else:
+        return JsonResponse({'error': 'Invalid form submission'}, status=400)
