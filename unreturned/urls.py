a6d51def8ac5e56b8af7d084108350c62bcc6ca3
West <Javeloza.Westin@gmail.com>
2024-03-25 16:41:58 +0800

    update

--- unreturned/urls.py
+++ unreturned/urls.py
@@ -4,9 +4,6 @@ from . import views
 urlpatterns = [
     path("unreturned", views.unreturned, name="unreturned"),
     path("admin_unreturned", views.admin_unreturned, name="admin_unreturned"),
-    path('admin_search_unreturned', views.admin_search_unreturned, name='admin_search_unreturned'),
-    path('faculty_search_unreturned', views.faculty_search_unreturned, name='faculty_search_unreturned'),
-    path('get_return_form/', views.get_return_form, name='get_return_form'),
-    path('save_return_form/', views.save_return_form, name='save_return_form'),
-    path('get_borrow_info/', views.get_borrow_info, name='get_borrow_info'),
+    path('return_item/<item_id>/<item_stock_id>/<borrow_form_id>', views.return_item, name="return_item"),
+    path('admin_return_item/<item_id>/<item_stock_id>/<borrow_form_id>', views.admin_return_item, name="admin_return_item")
 ]
--- unreturned/views.py
+++ unreturned/views.py
@@ -1,30 +1,13 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
-from .forms import *
 from recents.models import *
 from home.models import *
 from datetime import datetime
 from home.decorators import allowed_users
-from django.db.models import Q  # Import Q for complex queries
-from django.contrib.auth.decorators import login_required
-from django.http import JsonResponse
-from django.template.loader import render_to_string
-from rest_framework import serializers
-from django.core.exceptions import ObjectDoesNotExist
+from home.decorators import allowed_users
 
 # Create your views here.
-class BorrowSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Borrowed_Item
-        fields = '__all__' 
-        depth = 1  
-
-class UnreturnedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Unreturned_Item
-        fields = '__all__' 
-        depth = 1  
 
 @allowed_users(allowed_roles=['Faculty'])
 def unreturned(request):
@@ -41,94 +24,50 @@ def admin_unreturned(request):
     return render(request, 'unreturned/admin_unreturned.html',
                 {'unreturned_items': unreturned_items})
 
-def get_borrow_info(request):
-    borrow_pk = request.GET.get('borrow_id')
-    
-    try:
-        borrow_info = Borrowed_Item.objects.get(borrow_form_id=borrow_pk)
-        unreturned_info = Unreturned_Item.objects.get(item_borrowed=borrow_pk)
-        serialized_data = BorrowSerializer(borrow_info).data
-        unreturned_serialized_data = UnreturnedSerializer(unreturned_info).data
-        return JsonResponse({'borrowed_info': serialized_data,
-                             'unreturned_info': unreturned_serialized_data,})
-    except ObjectDoesNotExist:
-        return JsonResponse({'error': 'User not found'}, status=404)
-    
-def get_return_form(request):
-    return_form = ReturnForm()
-    form_html = render_to_string('unreturned/return_form.html', {'return_form': return_form}, request=request)
-    return JsonResponse({'form_html' : form_html})
+def return_item(request, item_id, item_stock_id, borrow_form_id):
 
-@login_required
-def save_return_form(request):
-    return_form = ReturnForm(request.POST)
-    stock_id = request.POST.get('stock_id')
-    unreturned_id = request.POST.get('unreturned_id')
-    borrowed_id = request.POST.get('borrowed_id')
-    if return_form.is_valid():
-        borrowed_id = int(borrowed_id)
-        borrowed_instance = get_object_or_404(Borrowed_Item, borrow_form_id=borrowed_id)
-        
-        item_borrowed_value = borrowed_instance.item_quantity
-        total_returned = return_form.cleaned_data['item_pristine_quantity'] + return_form.cleaned_data['item_damaged_quantity']
-        if item_borrowed_value == total_returned:
-            pristine_quantity = return_form.cleaned_data['item_pristine_quantity']
-            damaged_quantity = return_form.cleaned_data['item_damaged_quantity']
-            stock_id = int(stock_id)
-            unreturned_id = int(unreturned_id)
-            stock_instance = get_object_or_404(Stock, pk=stock_id)
-            unreturned_instance = get_object_or_404(Unreturned_Item, pk=unreturned_id)
-            stock_instance.item_current_quantity += pristine_quantity
-            stock_instance.item_pristine_quantity += pristine_quantity
-            stock_instance.item_borrowed_quantity -= item_borrowed_value
-            stock_instance.item_damaged_quantity += damaged_quantity
-        
-            stock_instance.save()
-            
-            unreturned_instance.delete()
-            print('updated current count')
+    pristine_no = request.POST.get("pristine")
+    damaged_no = request.POST.get("damaged")
 
-            # Return a success response
-            return JsonResponse({'message': 'Form submitted successfully'})
-        
-        else:
-            print('Form is NOT valid! Borrowed value greater than current stock')
-            return JsonResponse({'error': 'Invalid form submission'}, status=400)
+    unreturned_items = Unreturned_Item.objects.get(pk=item_id)
+    unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + unreturned_items.item_borrowed.item_quantity
+    unreturned_items.item_borrowed.item_stock.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity + int(pristine_no)
+    unreturned_items.item_borrowed.item_stock.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity + int(damaged_no)
+    unreturned_items.save()
 
-    else:
-        print('Form is NOT valid!')
-        print('Errors:', return_form.errors.as_data())
+    borrowed_item = Borrowed_Item.objects.get(pk=borrow_form_id)
+    borrowed_item.item_returned = True
+    borrowed_item.save()
 
-        return JsonResponse({'error': 'Invalid form submission'}, status=400)
+    item = Stock.objects.get(pk = item_stock_id)
+    item.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity
+    item.save()
 
-@allowed_users(allowed_roles=['Admin'])
-def admin_search_unreturned(request):
-    query = request.GET.get('q', '')
+    unreturned_items.delete()
 
-    if query:
-        # Adjust this query based on your model relationships
-        items = Unreturned_Item.objects.filter(
-            Q(item_borrowed__item_stock__item_information__item_name__icontains=query) |
-            Q(item_borrowed__item_borrower__username__icontains=query)
-        )
-    else:
-        items = Unreturned_Item.objects.all()
+    return redirect('unreturned')
 
-    return render(request, 'unreturned/admin_unreturned.html', {'unreturned_items': items, 'query': query})
+def admin_return_item(request, item_id, item_stock_id, borrow_form_id):
 
+    pristine_no = request.POST.get("pristine")
+    damaged_no = request.POST.get("damaged")
 
-@allowed_users(allowed_roles=['Faculty'])
-def faculty_search_unreturned(request):
-    query = request.GET.get('q', '')
-    user = request.user
+    unreturned_items = Unreturned_Item.objects.get(pk=item_id)
+    unreturned_items.item_borrowed.item_stock.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity + int(pristine_no)
+    unreturned_items.item_borrowed.item_stock.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity + int(pristine_no)
+    unreturned_items.item_borrowed.item_stock.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity + int(damaged_no)
+    unreturned_items.save()
 
-    if query:
-        # Adjust this query based on your model relationships
-        items = Unreturned_Item.objects.filter(
-            Q(item_borrowed__item_stock__item_information__item_name__icontains=query) |
-            Q(item_borrowed__item_borrower__username__icontains=query)
-        )
-    else:
-        items = Unreturned_Item.objects.filter(item_borrowed__item_borrower=user)
+    borrowed_item = Borrowed_Item.objects.get(pk=borrow_form_id)
+    borrowed_item.item_returned = True
+    borrowed_item.save()
 
-    return render(request, 'unreturned/unreturned.html', {'unreturned_items': items, 'query': query})
+    item = Stock.objects.get(pk = item_stock_id)
+    item.item_current_quantity = unreturned_items.item_borrowed.item_stock.item_current_quantity
+    item.item_pristine_quantity = unreturned_items.item_borrowed.item_stock.item_pristine_quantity
+    item.item_damaged_quantity = unreturned_items.item_borrowed.item_stock.item_damaged_quantity
+    item.save()
+
+    unreturned_items.delete()
+
+    return redirect('admin_unreturned')
